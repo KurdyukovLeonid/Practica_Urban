c2163d137a9454d8c57e36044e9f217ea0591201
Kurdyukov Leonid <leonid.kurdyukov@icloud.com>
2024-03-11 17:26:52 +0300

    Списки и словари

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['Арбуз', 'Персик', 'Ананас', 'Гранат', 'Банан', 'Яблоко']
+
+print('List:', my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2] = 'Личи'
+print('Modified list:', my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+
+print('\nDictionary:', my_dict)
+print('Translation:', my_dict['orange'])
+my_dict['kiwi'] = 'киви'
+print('Modified dictinary:', my_dict)
