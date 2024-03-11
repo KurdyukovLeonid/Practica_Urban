6118e30e1a5721db4cb5777f01236b46b55987fd
Kurdyukov Leonid <leonid.kurdyukov@icloud.com>
2024-03-11 17:05:55 +0300

    Строки и индексация строк

---
+++ HM-1.py
@@ -0,0 +1,11 @@
+its_my_line = 'это моя строка'
+its_new_line = 'это новая строка'
+
+print(its_my_line[0])
+print(its_my_line[-1])
+print(its_my_line[2:5])
+print(its_my_line[::-1])
+print(len(its_my_line))
+print(its_new_line + its_my_line)
+
+# print(its_new_line, 'и', its_my_line) - покрасивее выглядит
