f5b0bc4e414b5d4e63078b1dbcb5bb157d968d4f
Kurdyukov Leonid <leonid.kurdyukov@icloud.com>
2024-03-12 13:06:10 +0300

    Функции в Python.Функция с параметром

---
+++ function.py
@@ -0,0 +1,9 @@
+def print_params(arg):
+    print(arg)
+    print(arg)
+
+print_params('My name is Leonid')
+print()
+print_params('I live in Ivanovo')
+print()
+print_params('I love Urban University')
