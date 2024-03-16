164aa10f1e11c2f9bb861a8cd09f3aebf019774d
kurdyukov <leonid.kurdyukov@icloud.com>
2024-03-16 17:08:15 +0300

    Доработанная практика по факториалу

--- recursion.py
+++ recursion.py
@@ -6,9 +6,10 @@ par = 12, False, 5.6, 'строка'
 test(par)
 
 def factorial(n):
-    if n == 1:
+    if n <= 1:
         return 1
     fact_n = factorial(n-1)
     return n * fact_n
 
-print(factorial(9))
+print()
+print(factorial(0))
