2b4ac619d98e6d8792da86514797900986caedc6
kurdyukov <leonid.kurdyukov@icloud.com>
2024-05-08 09:44:06 +0300

    Наследование классов(исправление)

--- class_inheritance.py
+++ class_inheritance.py
@@ -2,15 +2,21 @@ class Car:
     def __init__(self):
         self.price = 1000000
     def horse_powers(self, power):
-        self.power = power
         return power
 
 class Nissan(Car):
     def __init__(self):
         self.price = 1200000
+
+    def horse_powers(self, power):
+        return power
 class Kia(Car):
     def __init__(self):
         self.price = 1500000
+
+    def horse_powers(self, power):
+        return power
+
 
 nissan = Nissan()
 print(f'Цена автомобиля Ниссан: {nissan.price}, кол-во л/с {nissan.horse_powers(180)}')
