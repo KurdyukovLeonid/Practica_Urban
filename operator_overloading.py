12c50c0ca2a338d4df07f47ca4100e9fe909cc94
kurdyukov <leonid.kurdyukov@icloud.com>
2024-05-10 03:29:30 +0300

    Множественное наследование(исправление)

--- operator_overloading.py
+++ operator_overloading.py
@@ -1,12 +1,11 @@
 class Building:
 
-    def __init__(self):
-        self.numberOfFloors = 5
-        self.buildingType = 'stone'
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
     def __eq__(self, other):
-        return self.numberOfFloors == other.numberOfFloors
-        return self.buildingType == other.buildingType
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
-build = Building
-build.__eq__()
+build = Building(10, 'stone')
+print(build == build)
